6a5e5bc91e1beaf9a0ac522a063c4263bef7fcdd
WUPA2 <panda.wu@oocl.com>
2019-07-29 09:18:44 +0800

    fix:show order delay bug

--- src/store/store.js
+++ src/store/store.js
@@ -9,7 +9,7 @@ const store = new Vuex.Store({
   state: {
     staffs: [],
     parkingLots: {},
-    parkingOrders: {},
+    parkingOrders: {content:[]},
     characters: [],
     demo: 'demo'
   },
